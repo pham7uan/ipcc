048a1fe61028abb1aa5bdc04a0f43bc0ded90fcf
pham7uan <phamtuannp@gmail.com>
2017-10-25 09:33:23 +0700

    First commit

---
+++ src/main/java/com/ssdc/ipcc/IpccApplication.java
@@ -0,0 +1,12 @@
+package com.ssdc.ipcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpccApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ssdc/ipcc/IpccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ssdc.ipcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpccApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
